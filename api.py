8afd9ba6802adde39cd242a84848e709a0b171fb
Vignesh-Desmond <vignesh.desmond@gmail.com>
2021-10-01 20:30:38 +0530

    minor changes

--- api.py
+++ api.py
@@ -4,19 +4,22 @@ from io import BytesIO
 from image2ascii import img2asci
 
 app = FastAPI()
+
+
 def insert_newlines(string, every=64):
-    return '\n'.join(string[i:i+every] for i in range(0, len(string), every))
+    return "\n".join(string[i : i + every] for i in range(0, len(string), every))
+
 
 @app.get("/")
 async def home():
     return {"home": "Welcome to Image2ascii API"}
 
+
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    a=await file.read()
-    image=PIL.Image.open(BytesIO(a))
+    a = await file.read()
+    image = PIL.Image.open(BytesIO(a))
     character, width = img2asci(image)
     print(type(character))
-    output={"op":insert_newlines(character, width)}
+    output = {"op": insert_newlines(character, width)}
     return output
-
